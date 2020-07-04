d0231cf04f2e30365c657d3fa5a5846f0848ead1
Lin Ying-Hsuan <wish7912@gmail.com>
2020-07-04 15:05:56 +0800

    use doc/

--- app.js
+++ app.js
@@ -4,25 +4,25 @@ const app = express();
 
 app.listen(3000, function () {
     app.get('/', function (req, res) {
-        res.sendFile('index.html', { root: __dirname + '/dist' });
+        res.sendFile('index.html', { root: __dirname + '/doc' });
     });
 
     // static files
-    app.use('/config', express.static('dist/config'));
-    app.use('/js', express.static('dist/js'));
-    app.use('/js', express.static(__dirname + '/node_modules/axios/dist/'));
+    app.use('/config', express.static('doc/config'));
+    app.use('/js', express.static('doc/js'));
+    app.use('/js', express.static(__dirname + '/node_modules/axios/doc/'));
     app.use('/js', express.static(__dirname + '/node_modules/lodash/'));
     app.use('/js', express.static(__dirname + '/node_modules/moment/'));
-    app.use('/css', express.static('dist/css'));
-    app.use('/data', express.static('dist/data'));
-    app.use('/images', express.static('dist/images'));
-    app.use('/fonts', express.static('dist/fonts'));
+    app.use('/css', express.static('doc/css'));
+    app.use('/data', express.static('doc/data'));
+    app.use('/images', express.static('doc/images'));
+    app.use('/fonts', express.static('doc/fonts'));
     console.log('open your explorer and go to localhost:3000');
 
     //404 handle
     app.use(function (req, res, next) {
         res.status(404);
-        res.sendFile('not_found.html', { root: __dirname + '/dist' });
+        res.sendFile('not_found.html', { root: __dirname + '/doc' });
     });
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -11,38 +11,38 @@ gulp.task('html', function () {
             prefix: '@@',
             basepath: '@file'
         }))
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest('doc/'));
 });
 
 gulp.task('images', function () {
     return gulp.src('src/assets/images/*.*')
-        .pipe(gulp.dest('dist/images'));
+        .pipe(gulp.dest('doc/images'));
 });
 
 gulp.task('data', function () {
     return gulp.src('src/assets/data/*.*')
-        .pipe(gulp.dest('dist/data'));
+        .pipe(gulp.dest('doc/data'));
 });
 
 gulp.task('config', function () {
     return gulp.src('src/assets/config/**/*.js')
         // .pipe(concat('custom.js')) // 將多個js檔串成一個，打包起來輸出的檔案名稱
         .pipe(terser()) // uglify
-        .pipe(gulp.dest('dist/config')); // 輸出路徑'
+        .pipe(gulp.dest('doc/config')); // 輸出路徑'
 });
 
 gulp.task('js', function () {
     return gulp.src('src/assets/javascripts/**/*.js')
         // .pipe(concat('custom.js')) // 將多個js檔串成一個，打包起來輸出的檔案名稱
         .pipe(terser()) // uglify
-        .pipe(gulp.dest('dist/js')); // 輸出路徑'
+        .pipe(gulp.dest('doc/js')); // 輸出路徑'
 });
 
 gulp.task('js_package', function () {
-    return gulp.src(['node_modules/axios/dist/axios.js', 'node_modules/moment/moment.js'])
+    return gulp.src(['node_modules/axios/doc/axios.js', 'node_modules/moment/moment.js'])
         // .pipe(concat('custom.js')) // 將多個js檔串成一個，打包起來輸出的檔案名稱
         .pipe(terser()) // uglify
-        .pipe(gulp.dest('dist/js')); // 輸出路徑'
+        .pipe(gulp.dest('doc/js')); // 輸出路徑'
 });
 
 gulp.task('styles', function () {
@@ -50,7 +50,7 @@ gulp.task('styles', function () {
         .pipe(gulpSass({ // 編譯 Scss
             outputStyle: 'compressed'
         }))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('doc/css'));
 });
 
 gulp.task('watch', function () {
