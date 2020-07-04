0e2c8c8cbafcd561023607ec2c28f4eaaadf3cd4
Lin Ying-Hsuan <wish7912@gmail.com>
2020-07-04 15:19:40 +0800

    upload docs/

---
+++ docs/js/header_nav.js
@@ -0,0 +1,1 @@
+$((function(){$("header.hp .nav-icon").click((function(e){$("header.hp .nav-bar-mo").animate({left:"0"}),e.preventDefault()})),$("header.hp .nav-bar-mo .nav-close").click((function(e){$("header.hp .nav-bar-mo").animate({left:"-300px"}),e.preventDefault()}))}));
